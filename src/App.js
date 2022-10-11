5d4fa5d6d541198f241af8d8bbcee6c91ac63d39
Justin <juc81@pitt.edu>
2022-10-11 17:24:13 -0400

    boiler for interactive task tracker - hardcoded shoutouts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Follow @juctaposed on Twitter',
+        day: 'Now',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Check out this portfolio: juctaposed.netlify.app',
+        day: 'Now',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Build more React apps',
+        day: 'October 17th at 3:00pm',
+        reminder: false,
+    },
+  ])
+
+  //Add Task
+  const addTask = (task) => {
+    //create an id for user -> remove once connected to Mongo/DB
+    const id = Math.floor(Math.random() * 20000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+    console.log(task)
+    console.log(id)
+  }
+
+  // Delete a Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+    console.log('delete', id)
+  }
+
+
+  // Toggle Reminder on Double Click
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id 
+        ? { ...task, reminder: !task.reminder } 
+        : task
+        ))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header 
+        onAdd={()=> setShowAddTask(!showAddTask)}
+        showAdd={showAddTask} />
+      {/* ternary shortcut for no else statement - show/hide the ass task form */}
+      {showAddTask && 
+        <AddTask onAdd={addTask} 
+        />}
+      {tasks.length > 0 ? (
+        <Tasks 
+          tasks={tasks} 
+          onDelete={deleteTask} 
+          onToggle={toggleReminder} 
+        />
+      ) : ('No Tasks Available')}
     </div>
   );
 }
